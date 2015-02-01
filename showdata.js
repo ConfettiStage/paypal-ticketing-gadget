0d5eca09265fd6005630721995b468f7fa3d03c7
Adam Wilson <admiwi@Adams-MacBook-Pro.local>
2015-02-01 00:23:05 -0500

    ...

---
+++ showdata.js
@@ -0,0 +1,17 @@
+var showData = [
+      {
+        name: "Waiting for Godot",
+        studentDiscount: true,
+        times: [
+          "Friday 730",
+          "Saturday"
+        ]
+      },
+      {
+        name: "Water by the Spoonful",
+        times: [
+          "Friday",
+          "Saturday"
+        ]
+      }
+    ];
