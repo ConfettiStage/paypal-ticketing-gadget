df5f3d58f2366022bc7cf1e19eac9dc16155ce2e
Adam Wilson <admiwi@Adams-MacBook-Pro.local>
2015-01-31 23:58:24 -0500

    ...

--- ticketwindow.js
+++ ticketwindow.js
@@ -1,4 +1,20 @@
-
+var showData = [
+      {
+        name: "Waiting for Godot",
+        studentDiscount: true,
+        times: [
+          "Friday 730",
+          "Saturday"
+        ]
+      },
+      {
+        name: "Water by the Spoonful",
+        times: [
+          "Friday",
+          "Saturday"
+        ]
+      }
+    ];
 
 var ShowOptions = React.createClass({
   render: function () {
