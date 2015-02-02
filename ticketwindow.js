a1615383fa6904e71ce65dbf1aab4f0ca7564833
Adam Wilson <admiwi@Adams-MacBook-Pro.local>
2015-02-01 23:23:29 -0500

    ...

--- ticketwindow.js
+++ ticketwindow.js
@@ -10,9 +10,12 @@ var Show = React.createClass({displayName: "Show",
   },
   render: function () {
     var options = [];
-    this.props.data.forEach( function (show, object) {
-      options.push(React.createElement("option", {value: show}, show))
-    });
+    if (this.props.data) {
+      this.props.data.forEach( function (show, object) {
+        options.push(React.createElement("option", {value: show}, show))
+      });  
+    }
+    
     return (
       React.createElement("div", null, 
         React.createElement("div", null, 
@@ -85,46 +88,6 @@ var TicketWindow = React.createClass({displayName: "TicketWindow",
     );
   }
 });
-
-var showData = {
-  "Waiting for Godot" : {
-    paypal: "YVY6U9UXGS594",
-    tickets: [
-      { 
-        type: "Standard Ticket", 
-        desc: "General Admission - $15.00" 
-      },
-      { 
-        type: "Student Ticket", 
-        desc: "Student Admission - $10.00"
-      }
-    ],
-    dates: [
-      "Friday 730",
-      "Saturday"
-    ]
-  },
-
-  "Water by the Spoonful":
-  {
-    paypal: "DE8XKVFFVN2BL",
-    tickets: [
-      { 
-        type: "Standard Ticket", 
-        desc: "General Admission - $15.00" 
-      },
-      { 
-        type: "Vetern Ticket", 
-        desc: "Special Admission for Veterns Admission - $10.00"
-      }
-    ],
-    dates: [
-      "Friday",
-      "Saturday"
-    ]
-  }
-};
-
 
 React.render(
   React.createElement(TicketWindow, {data: showData}),
--- ticketwindow.jsx
+++ ticketwindow.jsx
@@ -10,9 +10,12 @@ var Show = React.createClass({
   },
   render: function () {
     var options = [];
-    this.props.data.forEach( function (show, object) {
-      options.push(<option value={show}>{show}</option>)
-    });
+    if (this.props.data) {
+      this.props.data.forEach( function (show, object) {
+        options.push(<option value={show}>{show}</option>)
+      });  
+    }
+    
     return (
       <div>
         <div>
@@ -85,46 +88,6 @@ var TicketWindow = React.createClass({
     );
   }
 });
-
-var showData = {
-  "Waiting for Godot" : {
-    paypal: "YVY6U9UXGS594",
-    tickets: [
-      { 
-        type: "Standard Ticket", 
-        desc: "General Admission - $15.00" 
-      },
-      { 
-        type: "Student Ticket", 
-        desc: "Student Admission - $10.00"
-      }
-    ],
-    dates: [
-      "Friday 730",
-      "Saturday"
-    ]
-  },
-
-  "Water by the Spoonful":
-  {
-    paypal: "DE8XKVFFVN2BL",
-    tickets: [
-      { 
-        type: "Standard Ticket", 
-        desc: "General Admission - $15.00" 
-      },
-      { 
-        type: "Vetern Ticket", 
-        desc: "Special Admission for Veterns Admission - $10.00"
-      }
-    ],
-    dates: [
-      "Friday",
-      "Saturday"
-    ]
-  }
-};
-
 
 React.render(
   <TicketWindow data={showData} />,
