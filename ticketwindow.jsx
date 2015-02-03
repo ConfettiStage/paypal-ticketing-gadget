39bc3645b7635705a43e87cb9e1e4b40209c8386
Adam Wilson <admiwi@Adams-MacBook-Pro.local>
2015-02-02 19:27:09 -0500

    ...

--- ticketwindow.jsx
+++ ticketwindow.jsx
@@ -85,7 +85,7 @@ React.render(
       <form xmlns="http://www.w3.org/1999/xhtml" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="paypal">
         <input type="hidden" name="cmd" value="_s-xclick" />
         <input name="currency_code" type="hidden" value="USD" />
-        <Show data={this.props.data}/>
+        <Show data={showData}/>
         <input alt="PayPal - The safer, easier way to pay online!" border="0" name="submit" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" type="image" />
       </form>,
   document.getElementById('ticketWindow')
