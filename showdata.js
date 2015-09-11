ce4a3de9bab84a55a29e1ddee6f57e48e8e2688a
Adam Wilson <admiwi@Adams-MacBook-Pro.local>
2015-09-11 16:10:44 -0400

    removed robinhood, added dates for confetti fest

--- showdata.js
+++ showdata.js
@@ -1,26 +1,5 @@
 var showData = [
   {
-    name: "Robin Hood and the King's Ransom",
-    paypal: "56KWR85N2AHEL",
-    tickets: [
-      {
-        type: "General Admission",
-        desc: "General Admission - $15.00"
-      },
-      {
-        type: "Student-Child Admission",
-        desc: "Child and Student Admission - $8.00"
-      }
-    ],
-    dates: [
-      "Friday, August 7th at 7:30PM",
-      "Saturday, August 8th at 7:30PM",
-      "Friday, August 14th at 7:30PM",
-      "Saturday, August 15th at 7:30PM",
-      "Sunday, August 16th at 2:00PM",
-    ]
-  },
-  {
     name: "Confetti Fest XI",
     paypal: "YVY6U9UXGS594",
     tickets: [
@@ -34,7 +13,13 @@ var showData = [
       }
     ],
     dates: [
-      "October at 7:30PM",
+      "October 2nd at 7:30PM",
+      "October 3rd at 7:30PM",
+      "October 4th at 2:00PM",
+      "October 8th at 7:30PM",
+      "October 9th at 7:30PM",
+      "October 10th at 7:30PM",
+      "October 11th at 2:00PM"
     ]
   },
   {
